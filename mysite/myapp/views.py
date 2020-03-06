582ec2faa4fc6b14503930d14c51b4a31dc7b2db
kevinokeke <kevinace24@gmail.com>
2020-03-06 09:43:30 -0800

    Assignment 3 Submission

--- mysite/myapp/views.py
+++ mysite/myapp/views.py
@@ -4,7 +4,10 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-    return render(request, "index.html")
+    variable = {
+        "body":"CINS465 Hello World"
+    }
+    return render(request, "index.html", context=variable)
     #return HttpResponse("CINS465 Hello World")
 
 def about(request):
